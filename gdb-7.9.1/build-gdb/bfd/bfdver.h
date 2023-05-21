73411677a3dff809646fb3154febd6747f17ce0e
prai94 <priyank.rai94@gmail.com>
2023-05-22 00:41:50 +0530

    may 20

---
+++ gdb-7.9.1/build-gdb/bfd/bfdver.h
@@ -0,0 +1,4 @@
+#define BFD_VERSION_DATE 20150513
+#define BFD_VERSION 225510000
+#define BFD_VERSION_STRING  "(GNU Binutils) " "2.25.51"
+#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
---
+++ gdb-7.9.1/build-gdb/gdb/version.c
@@ -0,0 +1,4 @@
+#include "version.h"
+const char version[] = "7.9.1";
+const char host_name[] = "x86_64-unknown-linux-gnu";
+const char target_name[] = "arm-buildroot-linux-uclibcgnueabi";
